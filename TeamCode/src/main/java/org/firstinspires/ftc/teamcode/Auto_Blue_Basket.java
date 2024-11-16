5fe7c9eace62511998f568ec152cdace3a9943e4
mwollnik99 <74638220+mwollnik99@users.noreply.github.com>
2024-11-16 09:32:39 -0600

    Update Auto_Blue_Basket.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Basket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Blue_Basket.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder;
 import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
-package org.firstinspires.ftc.teamcode;
+import package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
